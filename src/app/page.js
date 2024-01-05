5d39705a5dd71fbfd8331c1b34a844b3af1de44d
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-01-05 06:38:38 +0000

    fdgf

--- src/app/page.js
+++ src/app/page.js
@@ -36,25 +36,29 @@ export default function Home() {
   const eNegativo = numero => Number(numero) < 0 ? `(${numero})` : numero
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex space-x-4">
+    <main className="flex flex-col items-center justify-between p-4 md:p-8 lg:p-16 xl:p-24">
+
+      <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
         <input type="number" className="rounded border px-2 py-1" placeholder="b" onChange={handleB} />
         <input type="number" className="rounded border px-2 py-1" placeholder="r" onChange={handleR} />
         <input type="number" className="rounded border px-2 py-1" placeholder="v" onChange={handleV} />
       </div>
-      <div className="mt-10 flex text-9xl">
+
+      <div className="mt-10 flex text-4xl md:text-6xl lg:text-8xl xl:text-9xl">
         <span>{b}</span>
         &lambda;
         <span>{r}</span>
-        <span className="mt-10">{Number(v) === 0 ? '' : v}</span>
+        <span className="mt-1 md:mt-0">{Number(v) === 0 ? '' : v}</span>
       </div>
-      <div className="mt-10 flex flex-1 flex-col text-xl text-center font-mono">
+
+      <div className="mt-10 flex flex-1 flex-col text-sm md:text-base lg:text-lg xl:text-xl text-center font-mono">
         <span>{b} × ({b}² - {r}² + {eNegativo(v)} × ({v > 0 ? `${b} - ${r}` : `${b} + ${r}`}))</span>
         <span>{b} × ({Math.pow(b, 2)} - {Math.pow(r, 2)} + {eNegativo(v)} × {v > 0 ? Number(b) - Number(r) : Number(b) + Number(r)})</span>
         <span>{b} × ({Math.pow(b, 2) - Math.pow(r, 2)} + {eNegativo(v * (v > 0 ? Number(b) - Number(r) : Number(b) + Number(r)))})</span>
         <span>{b} × {Math.pow(b, 2) - Math.pow(r, 2) + v * (v > 0 ? Number(b) - Number(r) : Number(b) + Number(r))}</span>
         <span>{barv(Number(b), Number(r), Number(v))}</span>
       </div>
+
     </main>
   )
 }
