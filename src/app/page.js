53a3c887885abbecc1a69f8217ea9e0d57239a3d
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-01-05 06:10:49 +0000

    ][[

--- src/app/page.js
+++ src/app/page.js
@@ -44,8 +44,8 @@ export default function Home() {
         <span className="mt-10">{v}</span>
       </div>
       <div className="mt-10 flex flex-1 flex-col text-xl">
-        <span>{b} × ({b}² - {r}² + {v} × ({v > 0 ? `${b} - ${r}` : `${b} + ${r}`}))</span>
-        <span>{b} × ({Math.pow(b, 2)} - {Math.pow(r, 2)} + {v} × ({v > 0 ? b - r : b + r}))</span>
+        <span>{b} × ({b}² - {r}² + {v < 0 ? `(${v})` : v} × ({v > 0 ? `${b} - ${r}` : `${b} + ${r}`}))</span>
+        <span>{b} × ({Math.pow(b, 2)} - {Math.pow(r, 2)} + {v} × {v > 0 ? b - r : b + r})</span>
         <span>{barv(b, r, v)}</span>
       </div>
     </main>
