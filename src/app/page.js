6f71e9aa66edc818dd38943a8839988ab43c1f72
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-01-05 05:02:48 +0000

    change

--- src/app/page.js
+++ src/app/page.js
@@ -8,35 +8,9 @@ export default function Home() {
   const [r, setR] = useState(1);
   const [v, setV] = useState(0);
 
-  const [temporizador, setTemporizador] = useState(null);
-
-  const handleB = (event) => {
-    clearTimeout(temporizador);
-
-    const novoTemporizador = setTimeout(() => {
-      setB(event.target.value);
-    }, 500);
-
-    setTemporizador(novoTemporizador);
-  };
-  const handleR = (event) => {
-    clearTimeout(temporizador);
-
-    const novoTemporizador = setTimeout(() => {
-      setR(event.target.value);
-    }, 500);
-
-    setTemporizador(novoTemporizador);
-  };
-  const handleV = (event) => {
-    clearTimeout(temporizador);
-
-    const novoTemporizador = setTimeout(() => {
-      setV(event.target.value);
-    }, 500);
-
-    setTemporizador(novoTemporizador);
-  };
+  const handleB = (event) => setB(event.target.value);
+  const handleR = (event) => setR(event.target.value);
+  const handleV = (event) => setV(event.target.value);
 
   useEffect(() => {
     return () => {
@@ -47,9 +21,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex space-x-4">
-        <input type="number" className="rounded border px-2 py-1" placeholder="b" value={b} onBlur={handleB} />
-        <input type="number" className="rounded border px-2 py-1" placeholder="r" value={r} onBlur={handleR} />
-        <input type="number" className="rounded border px-2 py-1" placeholder="v" value={v} onBlur={handleV} />
+        <input type="number" className="rounded border px-2 py-1" placeholder="b" value={b} onChange={handleB} />
+        <input type="number" className="rounded border px-2 py-1" placeholder="r" value={r} onChange={handleR} />
+        <input type="number" className="rounded border px-2 py-1" placeholder="v" value={v} onChange={handleV} />
       </div>
       <div className="mt-10 flex text-9xl">
         <span>{b}</span>
