7bdeec695c84d424c33e68dbf82269e3f67cb2fc
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-01-05 03:36:59 +0000

    ts

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
-      <div className="flex">
-        b&lambda;rv
+      <div className="flex text-9xl">
+        <span contenteditable>b</span>
+        &lambda;
+        <span contenteditable>r</span>
+        <span className="mt-10" contenteditable>v</span>
       </div>
     </main>
   )
