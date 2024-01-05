00afdef35e16a6647826b42c852ef23c553820e9
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-01-05 03:44:08 +0000

    vv

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,26 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex space-x-4">
-        <input type="text" className="px-2 py-1 border rounded" placeholder='b' />
-        <input type="text" className="px-2 py-1 border rounded" placeholder='r' />
-        <input type="text" className="px-2 py-1 border rounded" placeholder='v' />
-      </div>
-      <div className="flex text-9xl">
-        <span>b</span>
-        &lambda;
-        <span>r</span>
-        <span className="mt-10">v</span>
-      </div>
-    </main>
+  <div className="flex space-x-4">
+    <input type="number" className="rounded border px-2 py-1" placeholder="b" />
+    <input type="number" className="rounded border px-2 py-1" placeholder="r" />
+    <input type="number" className="rounded border px-2 py-1" placeholder="v" />
+  </div>
+  <div className="mt-10 flex text-9xl">
+    <span>b</span>
+    &lambda;
+    <span>r</span>
+    <span className="mt-10">v</span>
+  </div>
+  <div className="mt-10 flex flex-1 flex-col text-xl">
+    <span>Resolução</span>
+    <span>Resolução</span>
+    <span>Resolução</span>
+    <span>Resolução</span>
+    <span>Resolução</span>
+    <span>Resolução</span>
+    <span>Resolução</span>
+  </div>
+</main>
   )
 }
