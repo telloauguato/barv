515d3be820857d11ead7dcc04484da470ce8c572
Marcelo Augusto <37305715+telloauguato@users.noreply.github.com>
2024-01-05 03:40:06 +0000

    brv

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,16 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex space-x-4">
+        <input type="text" className="px-2 py-1 border rounded" placeholder='b' />
+        <input type="text" className="px-2 py-1 border rounded" placeholder='r' />
+        <input type="text" className="px-2 py-1 border rounded" placeholder='v' />
+      </div>
       <div className="flex text-9xl">
-        <span contenteditable>b</span>
+        <span>b</span>
         &lambda;
-        <span contenteditable>r</span>
-        <span className="mt-10" contenteditable>v</span>
+        <span>r</span>
+        <span className="mt-10">v</span>
       </div>
     </main>
   )
